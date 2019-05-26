e7a2eafefb30ca68868edce7692045f2d4fe70d9
Clyde Dcruz <clyde_dcruz@persistent.co.in>
2019-05-26 14:04:33 +0530

    have only built artifacts in master

---
+++ precache-manifest.44150d43c018ecd4e4d9ac1b7e36d664.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d582759ccd5d4fd5d71145d93dd9cfbe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef1f9d8396102a3176da",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "be6ea9eb6e8f350fa677",
+    "url": "/static/css/main.8767894b.chunk.css"
+  },
+  {
+    "revision": "ef1f9d8396102a3176da",
+    "url": "/static/js/2.f5fd96ac.chunk.js"
+  },
+  {
+    "revision": "be6ea9eb6e8f350fa677",
+    "url": "/static/js/main.528f5590.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
